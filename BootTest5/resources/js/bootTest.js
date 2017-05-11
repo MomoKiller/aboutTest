2d8a14067c64c2343713aad11482d3d50af737a9
MomoKiller <1226421458@qq.com>
2017-05-11 13:51:12 +0800

    add texiao ahaha

--- BootTest5/resources/js/bootTest.js
+++ BootTest5/resources/js/bootTest.js
@@ -12,3 +12,10 @@ $("div.picCenter img").hover(
         $(this).attr("src","resources/images/bootTest/"+picIndex+"NO.png");
     }
 );
+//定义按钮组的选择事件
+$("#btnRight button").click(
+    function(e){
+        $("#btnRight button").attr("class","btn btn-default");
+        $(this).addClass("btn-success active");
+    }
+);
